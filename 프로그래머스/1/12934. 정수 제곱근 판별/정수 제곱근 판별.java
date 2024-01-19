4848ec28ed16076e14c924cd27585440af7baac3
hinata7010 <1234minseok@naver.com>
2024-01-19 12:29:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long num = (long) Math.sqrt(n);
+        if((num*num) == n)
+        {
+            return (num+1)*(num+1);
+        }
+        else {
+            return -1;
+        }
+    }
+}
