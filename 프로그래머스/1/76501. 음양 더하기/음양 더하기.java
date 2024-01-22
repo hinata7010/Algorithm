63fbff6f742b47c045ea978aa8d8ee0b6d730ff3
hinata7010 <1234minseok@naver.com>
2024-01-22 14:20:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.22 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; i++)
+        {
+            if(signs[i] == true)
+            {
+                answer = absolutes[i] + answer;
+            }
+            else
+            {
+                answer = answer - absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
